bf7508494cbfe7f0d31b184a6c7361ac6b1640a7
PeterKampete <peterkampete01@gmail.com>
2022-04-12 23:31:56 +0100

    redux toolkit store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Input from './components/input/Input';
 
 function App() {
   return (
     <div className="App">
-      helo
+      <Input />
     </div>
   );
 }
---
+++ src/components/input/Input.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./input.css";
+
+import { useDispatch } from "react-redux";
+import saveTodo from "../../features/todoSlice";
+
+const Input = () => {
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
+
+  const addTodo = () => {
+    console.log(`Adding ${input}`);
+
+    dispatch(saveTodo({
+      item:input,
+      done: false,
+      id: Date.now()
+    }));
+  };
+
+  return (
+    <div className="input">
+      <input
+        type="text"
+        value={input}
+        onchange={(e) => setInput(e.target.value)}
+      />
+      <button onclick={addTodo} type="button" className="">
+        Add
+      </button>
+    </div>
+  );
+};
+
+export default Input;
